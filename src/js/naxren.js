f993e690846729e64a6001bf9ceec35fbfa79827
mugisz <danyasarabura@gmail.com>
2023-03-26 22:10:48 +0300

    SPA Project

---
+++ src/js/naxren.js
@@ -0,0 +1,6 @@
+function burger(){
+	const buttons = document.querySelector(".burger_menu")
+	const menu = document.querySelector(".nav")
+	buttons.classList.toggle("active");
+	menu.classList.toggle("active");
+}
